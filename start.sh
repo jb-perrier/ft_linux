e92932bbb81c294a1d5c908d822af59bb8b9a4eb
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2023-12-19 23:00:52 +0100

    Add sudo / Update path

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ sudo apt update
 
 sudo apt install -y git
 
-git clone https://github.com/jb-perrier/ft_linux.git /home/ft_linux
+sudo git clone https://github.com/jb-perrier/ft_linux.git ~/ft_linux
 
-sudo bash /home/ft_linux/build.sh
+sudo bash ~/ft_linux/build.sh
