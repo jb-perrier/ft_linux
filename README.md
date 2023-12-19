521f561d3299a94782a40a70885e4978ea241a59
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2023-12-19 22:29:43 +0100

    Update

---
+++ README.md
@@ -0,0 +1,9 @@
+```sh
+sudo apt update
+
+sudo apt install -y git
+
+git clone https://github.com/jb-perrier/ft_linux.git /home/ft_linux
+
+sudo bash /home/ft_linux/build.sh
+```
---
+++ build.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt upgrade -y
+
+## CHAPTER 2
+# redirect sh to bash
+sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
+
+sudo apt install -y build-essential git bison m4 gawk texinfo
+
+
+
